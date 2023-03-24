8b43aeee5493262fbf4cd56979135dea8032ebe2
piotrwojdan <piotr.wojdan2001@gmail.com>
2023-03-24 10:38:23 +0100

    fix lista 5 nowa

---
+++ Lista5/restapi-ztw/src/main/java/pl/pwr/ztw/restapi_ztw/Hello.java
@@ -0,0 +1,11 @@
+package pl.pwr.ztw.restapi_ztw;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class Hello {
+    @RequestMapping("/")
+    public String run() {
+        return "Hello ! I'm, running";
+    }
+}
