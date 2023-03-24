5a56d5c39da2663db5a290e23f78b3b44d9771b0
piotrwojdan <piotr.wojdan2001@gmail.com>
2023-03-24 10:16:34 +0100

    lista 5 start

---
+++ Lista5/restapi_ztw/src/main/java/pl/pwr/ztw/restapi_ztw/RestapiZtwApplication.java
@@ -0,0 +1,13 @@
+package pl.pwr.ztw.restapi_ztw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiZtwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiZtwApplication.class, args);
+	}
+
+}
---
+++ Lista5/restapi_ztw/src/test/java/pl/pwr/ztw/restapi_ztw/RestapiZtwApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pwr.ztw.restapi_ztw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiZtwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
