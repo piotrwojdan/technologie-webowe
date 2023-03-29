0bdf7e401f2f10fd3be81cd9b53da33e57313ac4
piotrwojdan <piotr.wojdan2001@gmail.com>
2023-03-29 17:09:35 +0200

    idk co ja tu robie

---
+++ Lista6/books_reading_vue_app/src/components/Books.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Tytuł</th>
+                    <th>Liczba stron</th>
+                    <th>Autorzy</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="book in books_source" :key="book.id">
+                    <td>{{ book.title }}</td>
+                    <td>{{ book.pages }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "books-table",
+    props: {
+        books_source: Array,
+    },
+}
+</script>
+
+<style lang="">
+    
+</style>
