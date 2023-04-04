86233a9a29db2e84f64324e9ccf13814d1ca5f26
piotrwojdan <piotr.wojdan2001@gmail.com>
2023-04-04 19:07:33 +0200

    walidacja ksiazek

--- Lista6/books_reading_vue_app/src/components/CreateBook.vue
+++ Lista6/books_reading_vue_app/src/components/CreateBook.vue
@@ -3,20 +3,32 @@
         <div class="container d-flex justify-content-center">
             <div class="row">
                 <form @submit.prevent="handleSubmit">
-                <br>
+                    <br>
                     <div class="row mb-4">
-                            <label class="form-label text-center mb-4">Title</label>
-                            <input type="text" class="form-control" v-model="book.title"/>
+                        <label class="form-label text-center mb-4">Title</label>
+                        <input type="text" class="form-control" v-model="book.title"
+                            :class="{ 'has-error': submitting && invalidTitle }" @focus="clearStatus"
+                            @keypress="clearStatus" />
                     </div>
                     <div class="row mb-4">
-                            <label class="form-label text-center mb-4">Pages</label>
-                            <input type="number" class="form-control" v-model="book.pages"/>
-                        
+                        <label class="form-label text-center mb-4">Pages</label>
+                        <input type="number" class="form-control" v-model="book.pages"
+                            :class="{ 'has-error': submitting && invalidPages }" @focus="clearStatus"
+                            @keypress="clearStatus" />
                     </div>
                     <div class="row mb-4">
-
                         <label class="form-label">Authors</label>
-                        <select multiple class="form-select" v-model="book.authorID"><option v-for="author in this.authors" :key="author.id" :value="author.id">{{ author.name + ' ' + author.lastName}}</option></select>
+                        <select multiple class="form-select" v-model="book.authorID">
+                            <option v-for="author in this.authors" :key="author.id" :value="author.id"
+                                :class="{ 'has-error': submitting && invalidPages }" @focus="clearStatus"
+                                @keypress="clearStatus">{{ author.name + ' '
+                                    + author.lastName }}</option>
+                        </select>
+                    </div>
+                    <div class="d-flex justify-content-center">
+                        <p v-if="error && submitting" class="error-message">
+                            Please put all the necesary info in!
+                        </p>
                     </div>
                     <div class="d-flex justify-content-center">
                         <button class="btn bg-dark text-white mb-4 center">Send</button>
@@ -26,16 +38,19 @@
         </div>
     </div>
 </template>
+
 <script>
-import { throwStatement } from '@babel/types';
 import axios from 'axios'
 
 export default {
     data() {
         return {
+            submitting: false,
+            error: false,
+            success: false,
             book: {
                 title: '',
-                pages: '',
+                pages: 0,
                 authorID: [],
             },
             authors: [],
@@ -48,15 +63,50 @@ export default {
     },
     methods: {
         handleSubmit() {
+            this.submitting = true
+            this.clearStatus()
+
+            if (this.invalidTitle || this.invalidPages || this.invalidAuthors) {
+                this.error = true
+                return
+            }
+
             axios.post('http://127.0.0.1:8080/books/', this.book);
             alert("Dodano ksiazke!");
             this.$router.push("/books");
+        },
+        clearStatus() {
+            this.success = false
+            this.error = false
+        },
+    },
+    computed: {
+        invalidTitle() {
+            return this.book.title === ''
+        },
+        invalidPages() {
+            return this.book.pages === 0
+        },
+        invalidAuthors() {
+            return this.book.authorID.length === 0
         }
-    }
-    
+    },
 }
 </script>
 
-<style>
-    
+<style scoped> form {
+     margin-bottom: 2rem;
+ }
+
+ [class*='-message'] {
+     font-weight: 500;
+ }
+
+ .error-message {
+     color: #d33c40;
+ }
+
+ .success-message {
+     color: #32a95d;
+ }
 </style>
--- Lista6/books_reading_vue_app/src/components/EditBook.vue
+++ Lista6/books_reading_vue_app/src/components/EditBook.vue
@@ -3,20 +3,29 @@
         <div class="container d-flex justify-content-center">
             <div class="row">
                 <form @submit.prevent="handleSubmit">
-                <br>
+                    <br>
                     <div class="row mb-4">
-                            <label class="form-label text-center mb-4">Title</label>
-                            <input type="text" class="form-control" v-model="book.title"/>
+                        <label class="form-label text-center mb-4">Title</label>
+                        <input type="text" class="form-control" v-model="book.title"
+                            :class="{ 'has-error': submitting && invalidTitle }" @focus="clearStatus"
+                            @keypress="clearStatus" />
                     </div>
                     <div class="row mb-4">
-                            <label class="form-label text-center mb-4">Pages</label>
-                            <input type="number" class="form-control" v-model="book.pages"/>
-                        
+                        <label class="form-label text-center mb-4">Pages</label>
+                        <input type="number" class="form-control" v-model="book.pages"
+                            :class="{ 'has-error': submitting && invalidPages }" @focus="clearStatus"
+                            @keypress="clearStatus" />
                     </div>
                     <div class="row mb-4">
-
                         <label class="form-label">Authors</label>
-                        <label><p v-for="author in book.authors" :key="author.id">{{ author.name + ' ' + author.lastName}}</p></label>
+                        <label>
+                            <p v-for="author in book.authors" :key="author.id">{{ author.name + ' ' + author.lastName }}</p>
+                        </label>
+                    </div>
+                    <div class="d-flex justify-content-center">
+                        <p v-if="error && submitting" class="error-message">
+                            Please put all the necesary info in!
+                        </p>
                     </div>
                     <div class="d-flex justify-content-center">
                         <button class="btn bg-dark text-white mb-4 center">Send</button>
@@ -34,20 +43,33 @@ export default {
     name: 'book-form',
     data() {
         return {
+            submitting: false,
+            error: false,
+            success: false,
             book: {
                 title: '',
-                pages: '',
+                pages: 0,
                 authors: [],
             },
         }
     },
     methods: {
         handleSubmit() {
-            console.log('uruchomiono handleSubmit')
-            
+            this.submitting = true
+            this.clearStatus()
+
+            if (this.invalidTitle || this.invalidPages) {
+                this.error = true
+                return
+            }
+
             axios.put('http://127.0.0.1:8080/books/' + this.id, this.book)
             alert("Zapisano!");
             this.$router.push("/books");
+        },
+        clearStatus() {
+            this.success = false
+            this.error = false
         },
     },
 
@@ -61,5 +83,31 @@ export default {
             })
             .catch(error => console.error(error))
     },
+    computed: {
+        invalidTitle() {
+            return this.book.title === ''
+        },
+        invalidPages() {
+            return this.book.pages === 0
+        },
+    },
 }
 </script>
+
+<style scoped>
+form {
+    margin-bottom: 2rem;
+}
+
+[class*='-message'] {
+    font-weight: 500;
+}
+
+.error-message {
+    color: #d33c40;
+}
+
+.success-message {
+    color: #32a95d;
+}
+</style>
