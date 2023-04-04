e2b11c91d43b0dc6c0b986ff77edb0c09ec6f0f0
magda19s <ms20050112@gmail.com>
2023-04-04 17:19:17 +0200

    Pagination

--- Lista6/books_reading_vue_app/src/main.js
+++ Lista6/books_reading_vue_app/src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue/dist/vue.esm-bundler'
 import { createRouter, createWebHashHistory } from 'vue-router'
+import BootstrapVue3 from 'bootstrap-vue-3'
 
 import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+
 
 import HomePage  from './pages/Home'
 import Books  from './components/Books'
@@ -27,5 +29,6 @@ const router = createRouter({
 const app = createApp({})
 
 app.use(router)
+app.use(BootstrapVue3)
 
 app.mount('#app')
