c569c7e31cf07dc4c72c01a9faed0e88fe3dcf1b
piotrwojdan <piotr.wojdan2001@gmail.com>
2023-04-04 16:24:39 +0200

    crud books skonczony pagination zaczete

--- Lista6/books_reading_vue_app/src/main.js
+++ Lista6/books_reading_vue_app/src/main.js
@@ -8,31 +8,24 @@ import HomePage  from './pages/Home'
 import Books  from './components/Books'
 import Authors from './components/Authors'
 import EditBook from './components/EditBook'
-
-// 2. Define some routes
-// Each route should map to a component.
-// We'll talk about nested routes later.
+import CreateBook from './components/CreateBook'
 
 const routes = [
   { path: '/', component: HomePage },
   { path: '/books', component: Books },
   { path: '/authors', component: Authors },
-  { path: '/editBook/:id', component: EditBook, props:true}
+  { path: '/editBook/:id', component: EditBook, props:true},
+  { path: '/createBook', component: CreateBook},
 ]
 
-// 3. Create the router instance and pass the `routes` option
-// You can pass in additional options here, but let's
-// keep it simple for now.
 const router = createRouter({
-  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+
   history: createWebHashHistory(),
-  routes, // short for `routes: routes`
+  routes, 
 })
 
-// 5. Create and mount the root instance.
 const app = createApp({})
-// Make sure to _use_ the router instance to make the
-// whole app router-aware.
+
 app.use(router)
 
 app.mount('#app')
