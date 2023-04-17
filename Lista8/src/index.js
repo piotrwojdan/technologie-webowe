46f5db021b74344dfb52634f925fe90265e46bc7
piotrwojdan <piotr.wojdan2001@gmail.com>
2023-04-17 13:12:41 +0200

    dziala photo

--- Lista8/src/index.js
+++ Lista8/src/index.js
@@ -5,16 +5,6 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-const fs = require("fs");
-
-class Model {
-  constructor() {
-    this.conversations = [];
-  }
-}
-
-let model = new Model();
-
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
@@ -24,16 +14,13 @@ const users = [];
 io.on('connection', (socket) => {
   socket.on('user_join', (data) => {
     socket.username = data;
-    // here check in DB with sender, receiver if record not exist then create
+
     users.push({
       socketId: socket.id,
       userId: data,
     });
     socket.broadcast.emit('user_join', data);
   });
-
- 
- 
 
   socket.on('typing', () => {
     socket.broadcast.emit('typing', socket.username);
@@ -44,33 +31,13 @@ io.on('connection', (socket) => {
   });
 
   socket.on('chat_message', async (data) => {
-    // const a = {
-    //   sender: data.senderId,
-    //   receiver: data.receiverId,
-    //   type: 'TEXT',
-    //   message: data.message,
-    // };
     let msg = {message: data, username: socket.username, date: new Date().toLocaleString()}
-    // model.conversations.push(a);
     socket.broadcast.emit('chat_message', msg);
   });
 
   socket.on('photo_message', async (data) => {
-    // const a = {
-    //   sender: data.senderId,
-    //   receiver: data.receiverId,
-    //   type: 'TEXT',
-    //   message: data.message,
-    // };
-    let msg = {username: socket.username, date: new Date().toLocaleString()}
-    // model.conversations.push(a);
-    socket.broadcast.emit('photo', msg, function (response){
-      if (response.error){
-        console.log("Error: " + response.error);
-      } else {
-        console.log("Success");
-      }
-    });
+    let msg = {message: data,username: socket.username, date: new Date().toLocaleString()};
+    socket.broadcast.emit('photo_message', msg);
     
   });
 
@@ -78,17 +45,6 @@ io.on('connection', (socket) => {
     socket.broadcast.emit('user_leave', socket.username);
   });
 
-  
-
-  // socket.on('old_message', async (data) => {
-  //   const messageList = await model.conversations.findAll({
-  //     where: {
-  //       sender: data.senderId,
-  //       receiver: data.receiverId,
-  //     },
-  //   });
-  //   socket.broadcast.emit('getList', messageList);
-  // });
 });
 
 
