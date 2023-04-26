10bda056c274b303c8613e942b4c4d4dfc7b3ff7
piotrwojdan <piotr.wojdan2001@gmail.com>
2023-04-26 15:29:33 +0200

    started project

---
+++ PROJEKCIK/backend/CinemaReservations/src/test/java/pl/webowe/projekt/CinemaReservations/CinemaReservationsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.webowe.projekt.CinemaReservations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaReservationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
